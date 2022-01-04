2067525ce0479f5bd072dc83b8bb379312b71de9
siddhartha bajpai <bajpai.siddhartha.stp@gmail.com>
2022-01-04 22:18:49 +0530

    CE button

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,7 +5,7 @@ import "./Calculator.css"
 
 function Calculator() {
    const [s,setS] = useState("")
-    const [id,setId] = useState([0,1,2,3,4,5,6,7,8,9,10,11,12,13]);
+    const [id,setId] = useState([0,1,2,3,4,5,6,7,8,9,10,11,12]);
     const [buttons,setButtons]  = useState({
         0:'0',
         1:'1',
@@ -20,7 +20,7 @@ function Calculator() {
         10:'+',
         11:'*',
         12:'-',
-        13:'0',
+        
        
     })
     const btnclk =()=>
@@ -34,7 +34,8 @@ function Calculator() {
                  <span onClick={()=>setS("")} className="screen" ><textarea value={s}></textarea></span>
                 <div className = "button_container">
                   {id.map((m,i)=><button className = "buttons" onClick={()=>{setS(s=>s+buttons[i])}} ><h1>{`${buttons[i]}`}</h1></button>)}
-                  <button className = "buttons" onClick={()=>setS(s=>eval(s))}><h1>=</h1></button>
+                    <button className = "buttons" onClick={()=>setS("")}><h1>CE</h1></button>
+                 <button className = "buttons" onClick={()=>setS(s=>eval(s))}><h1>=</h1></button>
                 </div>
         </div>
     )
