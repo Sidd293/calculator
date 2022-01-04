b80181016bab84bf4d065dae825b2f4b215fb185
siddhartha bajpai <bajpai.siddhartha.stp@gmail.com>
2022-01-04 21:52:44 +0530

    files uploaded

---
+++ src/Calculator.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {useState,useEffect} from 'react';
+import "./Calculator.css"
+
+
+function Calculator() {
+   const [s,setS] = useState("")
+    const [id,setId] = useState([0,1,2,3,4,5,6,7,8,9,10,11,12,13]);
+    const [buttons,setButtons]  = useState({
+        0:'0',
+        1:'1',
+        2:'2',
+        3:'3',
+        4:'4',
+        5:'5',
+        6:'6',
+        7:'7',
+        8:'8',
+        9:'9',
+        10:'+',
+        11:'*',
+        12:'-',
+        13:'0',
+       
+    })
+    const btnclk =()=>
+    {
+
+    }
+    
+    return (
+        <div className = "calci">
+            <div className='txt'>CASIO</div>
+                 <span onClick={()=>setS("")} className="screen" ><textarea value={s}></textarea></span>
+                <div className = "button_container">
+                  {id.map((m,i)=><button className = "buttons" onClick={()=>{setS(s=>s+buttons[i])}} ><h1>{`${buttons[i]}`}</h1></button>)}
+                  <button className = "buttons" onClick={()=>setS(s=>eval(s))}><h1>=</h1></button>
+                </div>
+        </div>
+    )
+}
+
+export default Calculator
